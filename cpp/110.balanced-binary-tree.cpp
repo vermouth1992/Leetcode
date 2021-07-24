6b86f54e7a8bafe52f02f4b56d6e1268dbb09516
Chi Zhang <czhangseu@gmail.com>
2021-07-23 22:01:22 -0700

    110 cpp

---
+++ cpp/110.balanced-binary-tree.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=110 lang=cpp
+ *
+ * [110] Balanced Binary Tree
+ *
+ * https://leetcode.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (45.02%)
+ * Total Accepted:    603.8K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, determine if it is height-balanced.
+ * 
+ * For this problem, a height-balanced binary tree is defined as:
+ * 
+ * 
+ * a binary tree in which the left and right subtrees of every node differ in
+ * height by no more than 1.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1,2,2,3,3,null,null,4,4]
+ * Output: false
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = []
+ * Output: true
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is in the range [0, 5000].
+ * -10^4 <= Node.val <= 10^4
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include "common.hpp"
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        bool result = true;
+        this->height(root, result);
+        return result;
+    }
+
+private:
+    int height(TreeNode* root, bool &result) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int left = this->height(root->left, result);
+        if (!result) return -1; // height doesn't matter
+        int right = this->height(root->right, result);
+        if (!result) return -1; // height doesn't matter
+        if (abs(left - right) > 1) {
+            result = false;
+        }
+        return std::max(left, right) + 1;
+    }
+};
