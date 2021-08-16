694ccc3f8e46eb150f9cee1e57eca112080646fa
Chi Zhang <czhangseu@gmail.com>
2021-08-16 16:23:38 -0700

    303 cpp

---
+++ cpp/303.range-sum-query-immutable.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=303 lang=cpp
+ *
+ * [303] Range Sum Query - Immutable
+ *
+ * https://leetcode.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (49.01%)
+ * Total Accepted:    290.8K
+ * Total Submissions: 564.5K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n' +
+  '[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * Given an integer array nums, handle multiple queries of the following
+ * type:
+ * 
+ * 
+ * Calculate the sum of the elements of nums between indices left and right
+ * inclusive where left <= right.
+ * 
+ * 
+ * Implement the NumArray class:
+ * 
+ * 
+ * NumArray(int[] nums) Initializes the object with the integer array nums.
+ * int sumRange(int left, int right) Returns the sum of the elements of nums
+ * between indices left and right inclusive (i.e. nums[left] + nums[left + 1] +
+ * ... + nums[right]).
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * Output
+ * [null, 1, -1, -3]
+ * 
+ * Explanation
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+ * numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+ * numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^4
+ * -10^5 <= nums[i] <= 10^5
+ * 0 <= left <= right < nums.length
+ * At most 10^4 calls will be made to sumRange.
+ * 
+ * 
+ */
+
+#include "common.hpp"
+
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+        int sum = 0;
+        for (auto & num : nums) {
+            sum += num;
+            cumulative_sum.push_back(sum);
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        return sumRange(right) - sumRange(left - 1);
+    }
+
+private:
+    vector<int> cumulative_sum;
+
+    int sumRange(int right) {
+        if (right == -1) return 0;
+        else return cumulative_sum[right];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
